eebeb6ebf8d3e70dc8e6bf571dc018e4396b8be0
Patric Iskevi <patriciskevi@gmail.com>
2019-06-30 15:22:04 +0200

    added cache request to the sw.js to work offline

--- sw.js
+++ sw.js
@@ -1,14 +1,39 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/materialize.min.css',
+    '/css/styles.css',
+    '/img/user_avatar.svg',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
 // install service worker
 self.addEventListener('install', evt => {
-    console.log('service worker has been installed', evt)
+    // console.log('service worker has been installed', evt)
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-    console.log('service worker has been activated', evt)
+    // console.log('service worker has been activated', evt)
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt)
-})
+    // console.log('fetch event', evt)
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
+});
